d3494a9c8c1860c770da619edcf953a1a12c4284
Dylan Reuter <djreuter45@gmail.com>
2017-07-15 16:56:15 -0600

    added more of persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -21,5 +21,19 @@
 			has been around for five years and has been doing quite well. </p>
 		<p><strong>Technology: </strong> John is a Mac user and has a Macbook pro along with a slightly dated iPhone
 			6s. His office runs on a high speed wireless internet connection.</p>
+		<p><strong>Attitudes &amp; Behaviors</strong> John wants to be sure that he selects the right candidate. He
+			has not hired anyone in over a year and wants to be absolutely certain that he hires the right person for the
+			job. He is not in a hurry to hire someone so he is taking his time. John has been looking at resumes and online
+			portfolio sites for about 15 minutes each day for the past week.
+		</p>
+		<p><strong>Frustrations &amp; Needs</strong> John wants to hire an entry level full-stack developer to join
+			his team so experience isn't everything. He wants someone that is highly motivated and eager to learn, that
+			also has a solid foundation of both frontend and backend technologies that can be built on. John is
+			focusing on the whole package regarding the online portfolios of the candidates that have
+			applied. He is looking at the appearance and functionality of the candidates website, as well as the
+			projects it contains. So far he has not found anybody that has both an impressive online portfolio and the
+			right personality and soft skills to match. He has been looking at multiple candidates sites everyday for a
+			week and is starting to think that his ideal candidate might not exist.</p>
+
 	</body>
 </html>
