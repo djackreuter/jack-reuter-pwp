d7162dacf7b61312766795273405c1d7f6d41f7e
Dylan Reuter <djreuter45@gmail.com>
2017-07-15 16:00:34 -0600

    purpose, audience, and goals, along with the start of persona

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Milestone 1</title>
+	</head>
+	<body>
+		<h1>Milestone 1</h1>
+		<p><strong>Purpose:</strong> My personal website will be an online portfolio showcasing the work I've done as
+			a developer in order to help me gain employment from a company</p>
+		<p><strong>Audience:</strong> This site will be set up and designed for potential employers, hiring managers,
+			and/or senior developers.</p>
+		<p><strong>Goals:</strong> The goal of this site is to act as a gallery to showcase my work to potential
+			employers in order to gain full time employment.</p>
+		<h2>Persona</h2>
+		<p><strong>Name:</strong> John Johnson</p>
+		<p><strong>Age:</strong> 42</p>
+		<p><strong>Profession:</strong> John Johnson owns abqwebdevs.com, a software development company in
+			Albuquerque, NM. John's company assists business and organizations by developing software and
+			building websites to help them grow their businesses and develop a stronger online presence. Abq Web Devs
+			has been around for five years and has been doing quite well. </p>
+		<p><strong>Technology: </strong> John is a Mac user and has a Macbook pro along with a slightly dated iPhone
+			6s. His office runs on a high speed wireless internet connection.</p>
+	</body>
+</html>
