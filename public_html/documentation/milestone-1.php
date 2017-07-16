43cf0310799f3cf48c69c06e578f2c91aa53a064
Dylan Reuter <djreuter45@gmail.com>
2017-07-16 16:00:56 -0600

    finished persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -23,17 +23,26 @@
 			6s. His office runs on a high speed wireless internet connection.</p>
 		<p><strong>Attitudes &amp; Behaviors</strong> John wants to be sure that he selects the right candidate. He
 			has not hired anyone in over a year and wants to be absolutely certain that he hires the right person for the
-			job. He is not in a hurry to hire someone so he is taking his time. John has been looking at resumes and online
-			portfolio sites for about 15 minutes each day for the past week.
+			job. He wants to hire a junior developer within the next two weeks. John has been
+			looking at resumes and online portfolio sites for about 15 minutes each day for the past week.
 		</p>
 		<p><strong>Frustrations &amp; Needs</strong> John wants to hire an entry level full-stack developer to join
 			his team so experience isn't everything. He wants someone that is highly motivated and eager to learn, that
 			also has a solid foundation of both frontend and backend technologies that can be built on. John is
 			focusing on the whole package regarding the online portfolios of the candidates that have
 			applied. He is looking at the appearance and functionality of the candidates website, as well as the
-			projects it contains. So far he has not found anybody that has both an impressive online portfolio and the
-			right personality and soft skills to match. He has been looking at multiple candidates sites everyday for a
-			week and is starting to think that his ideal candidate might not exist.</p>
-
+			projects it contains. So far he has not found anybody that has both an impressive resume, online portfolio, and
+			the right personality and soft skills to match. He has been looking at multiple candidates sites everyday for a
+			week. With his two week deadline fast approaching, he is starting to think that his ideal candidate
+			might not exist.</p>
+		<p><strong>Goals:</strong> John's main concern is whether or not a potential candidate has the appropriate
+			foundation of knowledge and appropriate skills to be a valuable employee. John wants to look at the
+			candidates websites and know after 5 min of browsing the content, and judging the appearance of the overall
+			site, if this candidate is worthy of an interview. He is paying close attention to the functionality of the
+			site and thinks that it is a good way to get an idea of each developers skills and attention to detail.
+			John wants to find a site that is easy to navigate, has seamless functionality, and is not over complicated
+			for the user. If the site is too difficult to figure out how to navigate, John will spend no more than 30
+			seconds on it.</p>
+		<p><strong>Use Case:</strong> </p>
 	</body>
 </html>
