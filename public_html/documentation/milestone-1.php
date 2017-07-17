c47d4c8264b810d6184c0b1de10820568a0c71a1
Dylan Reuter <djreuter45@gmail.com>
2017-07-17 15:54:50 -0600

    corrected some grammar and re-worded some sections

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,7 +7,7 @@
 	<body>
 		<h1>Milestone 1</h1>
 		<p><strong>Purpose:</strong> My personal website will be an online portfolio showcasing the work I've done as
-			a developer in order to help me gain employment from a company</p>
+			a developer in order to help me gain employment from a company.</p>
 		<p><strong>Audience:</strong> This site will be set up and designed for potential employers, hiring managers,
 			and/or senior developers.</p>
 		<p><strong>Goals:</strong> The goal of this site is to act as a gallery to showcase my work to potential
@@ -15,7 +15,7 @@
 		<h2>Persona</h2>
 		<p><strong>Name:</strong> John Johnson</p>
 		<p><strong>Age:</strong> 42</p>
-		<p><strong>Profession:</strong> John Johnson owns abqwebdevs.com, a software development company in
+		<p><strong>Profession:</strong> John Johnson is the owner of abqwebdevs.com, a software development company in
 			Albuquerque, NM. John's company assists business and organizations by developing software and
 			building websites to help them grow their businesses and develop a stronger online presence. Abq Web Devs
 			has been around for five years and has been doing quite well. </p>
@@ -24,7 +24,7 @@
 		<p><strong>Attitudes &amp; Behaviors</strong> John wants to be sure that he selects the right candidate. He
 			has not hired anyone in over a year and wants to be absolutely certain that he hires the right person for the
 			job. He wants to hire a junior developer within the next two weeks. John has been
-			looking at resumes and online portfolio sites for about 15 minutes each day for the past week.
+			looking at resumes and online portfolio sites for about 10 minutes each day for the past week.
 		</p>
 		<p><strong>Frustrations &amp; Needs</strong> John wants to hire an entry level full-stack developer to join
 			his team so experience isn't everything. He wants someone that is highly motivated and eager to learn, that
@@ -37,22 +37,22 @@
 			week. With his two week deadline fast approaching, he is starting to think that his ideal candidate
 			might not exist.</p>
 		<p><strong>Goals:</strong> John's main concern is whether or not a potential candidate has the appropriate
-			foundation of knowledge and appropriate skills to be a valuable employee. John looks at the
-			candidates websites and know after 5 min of browsing the content, and judging the appearance of the overall
+			foundation of knowledge and the necessary skills to be a valuable employee. John looks at the
+			candidates websites and knows after 5 min of browsing the content, and judging the appearance of the overall
 			site, if this candidate is worthy of an interview. He is paying close attention to the functionality of the
 			site and thinks that it is a good way to get an idea of each developers skills and attention to detail.
 			John wants to find a site that is easy to navigate, has seamless functionality, and is not over complicated
-			for the user. If the site is too difficult to figure out how to navigate, John will spend no more than 30
-			seconds on it, cross that applicants name off the list and move on to the next one.</p>
+			for the user. If the site is too difficult to figure out how to navigate, or is poorly constructed, John will
+			spend	no more than 30 seconds on it, cross that applicants name off the list and move on to the next candidate.</p>
 		<p><strong>Use Case:</strong> It's Monday morning and John is looking over resumes and online
-			portfolio sites on his Macbook and getting ready to contact the candidates he believes would be a good fit for
-			the job. John comes across my resume and thinks that I look good on paper and he decides to look at my personal
-			website as well. John notices the professional layout of my site right away and has no problem navigating to
-			the different areas on the page. He likes the responsiveness of the site and has a look at the projects section
-			to see what else I've done. Still impressed after seeing what projects I've worked on, John decides to put me
-			down as a candidate to interview. He wants to set up in-person interviews on Tuesday and Wednesday so that
-			he can make his decision on who he wants to hire by the weekend. He has a total of 7 candidates to interview
-			starting at 10am Tuesday morning. He will interview 4 candidates on Tuesday and the remaining 3 on Wednesday.
-		</p>
+			portfolio sites on his Macbook and he is getting ready to contact the candidates he believes would be a good
+			fit for the position. John comes across my resume and thinks that I look good on paper and he decides to look
+			at my personal website as well. John notices the professional layout of my site right away and has no
+			problem navigating to the different areas on the page. He likes the responsiveness of the site and has a
+			look at the projects section to see what else I've done. Still impressed after seeing what projects I've
+			worked on, John decides to put me down as a candidate to interview. He wants to set up in-person interviews
+			on Tuesday and Wednesday so that he can make his decision on who he wants to hire by the weekend. He has a
+			total of 7 candidates to interview starting at 10am Tuesday morning. He will interview 4 candidates on
+			Tuesday and the remaining 3 on Wednesday.</p>
 	</body>
 </html>
