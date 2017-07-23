4d00b3e60b42db8eb8bda1725f57c42a8e426f19
Dylan Reuter <djreuter45@gmail.com>
2017-07-22 20:13:22 -0600

    added milestone-2 file and wrote basic content

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,34 @@
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Milestone 2</title>
+	</head>
+	<body>
+		<!-- Document the kind of content that you plan to feature on your site, and the purpose and strategy behind it
+		 -->
+		<h1>Content Strategy</h1>
+		<h3>Header</h3>
+		<p>The header of the site will contain my name as well as a small picture. It will also contain four navigation
+			buttons horizontally across the bottom that will direct the user to that section of the page when clicked.
+			The header will remain fixed at the top of the page. The navigation buttons will be:</p>
+		<ul>
+			<li>
+				Home
+			</li>
+			<li>
+				About
+			</li>
+			<li>
+				Projects
+			</li>
+			<li>
+				Contact
+			</li>
+		</ul>
+		<p>The <em>Home</em> button will bring the user back to the top of the page.</p>
+		<p>The <em>About</em> button will bring the user to a section containing a bio about me</p>
+		<p>The <em>Projects</em> button will bring the user to a section containing a gallery of projects that I've
+			done.</p>
+		<p>The <em>Contact</em> button will bring the user to the bottom of the page that will contain a contact form.</p>
+	</body>
+</html>
