dc89be2b024f4cbaf7735c538ec9d7d6f17fc340
Dylan Reuter <djreuter45@gmail.com>
2017-07-25 16:57:12 -0600

    added more content to milestone-2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,7 +8,7 @@
 		 -->
 		<h1>Content Strategy</h1>
 		<h3>Header</h3>
-		<p>The header of the site will contain my name as well as a small picture. It will also contain four navigation
+		<p>The header of the site will contain my name and also contain four navigation
 			buttons horizontally across the bottom that will direct the user to that section of the page when clicked.
 			The header will remain fixed at the top of the page. The navigation buttons will be:</p>
 		<ul>
@@ -25,10 +25,23 @@
 				Contact
 			</li>
 		</ul>
-		<p>The <em>Home</em> button will bring the user back to the top of the page.</p>
-		<p>The <em>About</em> button will bring the user to a section containing a bio about me</p>
+		<h3>Main Content</h3>
+		<p>The <em>Home</em> section will essentially be the top of the page. It will and have a picture of something
+			code related or a scenic picture of Albuquerque. The idea of the picture will be to add illustrations to
+			the page to show employers that I am a developer, or if I chose the scenic picture of Albuquerque, it will
+			show that I am invested in the community.</p>
+		<p>The <em>About</em> button will bring the user to a section containing a bio about me. It will contain a
+			small picture of me at the as well as a summary of me on both a personal and professional level. The idea
+			behind this is to give employers an idea of who I am outside of the scope of a resume.</p>
 		<p>The <em>Projects</em> button will bring the user to a section containing a gallery of projects that I've
-			done.</p>
-		<p>The <em>Contact</em> button will bring the user to the bottom of the page that will contain a contact form.</p>
+			done. It will contain the capstone, as well as other development projects. It will be used to showcase my
+			skills and capabilities to potential employers. It will also show employers all the projects I have been apart
+			of.</p>
+		<p>The <em>Contact</em> button will bring the user to the bottom of the page that will contain a contact form.
+			The form will be in place for a potential employer to contact me easily.</p>
+		<h3>Footer</h3>
+		<p>The last section will be the footer at the very bottom. This will contain links to my LinkedIn profile,
+			Twitter account, and GitHub profile. This will allow a potential employer to be able to find other relevant
+			pages about me without having to go searching.</p>
 	</body>
 </html>
