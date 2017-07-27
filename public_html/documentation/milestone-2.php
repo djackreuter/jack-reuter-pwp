b525cf3d0b64396e1d66b8cfca42fde3b97a3e5a
Dylan Reuter <djreuter45@gmail.com>
2017-07-27 15:30:26 -0600

    added wireframes and rewrote some html sections

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,16 +1,16 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>Milestone 2</title>
+		<title>milestone 2</title>
 	</head>
 	<body>
-		<!-- Document the kind of content that you plan to feature on your site, and the purpose and strategy behind it
+		<!-- document the kind of content that you plan to feature on your site, and the purpose and strategy behind it
 		 -->
 		<h1>Content Strategy</h1>
 		<h3>Header</h3>
-		<p>The header of the site will contain my name and also contain four navigation
-			buttons horizontally across the bottom that will direct the user to that section of the page when clicked.
-			The header will remain fixed at the top of the page. The navigation buttons will be:</p>
+		<p>The header of the site will contain my name and also contain four navigation buttons horizontally across the
+			bottom that will direct the user to that section of the page when clicked. The header will remain fixed at
+			the top of the page. The navigation buttons will be:</p>
 		<ul>
 			<li>
 				Home
@@ -28,19 +28,23 @@
 		<h3>Main Content</h3>
 		<p>The <em>Home</em> section will essentially be the top of the page. It will and have a picture of me working
 			on my laptop and a quote over the image. The idea of the picture will be to show employers that I am an
-			active developer</p>
+			active developer.</p>
 		<p>The <em>About</em> button will bring the user to a section containing a bio about me. It will contain a
-			small picture of me at the as well as a summary of me on both a personal and professional level. The idea
-			behind this is to give employers an idea of who I am outside the scope of a resume.</p>
+			small picture of me at the top, as well as a summary of me on both a personal and professional level. The
+			idea behind this is to give employers an idea of who I am outside the scope of a resume.</p>
 		<p>The <em>Projects</em> button will bring the user to a section containing a gallery of projects that I've
-			done. It will contain the capstone, as well as other development projects. It will be used to showcase my
-			skills and capabilities to potential employers. It will also show employers all the projects I have been apart
-			of.</p>
+			done. The images will be in the form of a link, if applicable, that takes the user to the website. If
+			there is no link, the image will just be a screenshot of the site. The profile section will contain the
+			capstone, as well	as other development projects. It will be used to showcase my skills and capabilities to
+			potential employers. It will also show employers all the projects I have been apart of.</p>
 		<p>The <em>Contact</em> button will bring the user to the bottom of the page that will contain a contact form.
 			The form will be in place for a potential employer to contact me easily.</p>
 		<h3>Footer</h3>
 		<p>The last section will be the footer at the very bottom. This will contain links to my LinkedIn profile,
-			Twitter account, and GitHub profile. This will allow a potential employer to be able to find other relevant
+			GitHub profile, and Twitter account. This will allow a potential employer to be able to find other relevant
 			sites about me without having to do additional searching.</p>
+		<img src="images/pwp-1.png" alt="desktop wireframe 1 of 2"/>
+		<img src="images/pwp-2.png" alt="desktop wireframe 2 of 2"/>
+		<img src="images/pwp-mobile.png" alt="mobile wireframe"/>
 	</body>
 </html>
