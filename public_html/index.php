21514ade066ae62f12b0d2aa861c7895b1e48021
Dylan Reuter <djreuter45@gmail.com>
2017-07-30 15:32:59 -0600

    started html for personal website

---
+++ public_html/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Jack Reuter | Online Portfolio</title>
+	</head>
+	<body>
+		<header>
+			<nav>
+				<h1>Jack Reuter</h1>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Projects</a></li>
+					<li><a href="#">Contact></a></li>
+				</ul>
+			</nav>
+			<p>
+				<!-- quote will go here -->
+			</p>
+		</header>
+		<main>
+			<section>
+				<!-- Bio for About section here -->
+			</section>
+		</main>
+	</body>
+</html>
