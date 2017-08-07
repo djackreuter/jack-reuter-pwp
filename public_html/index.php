cd34c5617b569929e50bbe720476bb8ceda3a0f2
Dylan Reuter <djreuter45@gmail.com>
2017-08-07 09:26:28 -0600

    added navbar and background color

--- public_html/index.php
+++ public_html/index.php
@@ -18,6 +18,7 @@
 				rel="stylesheet"/>
 
 		<!-- Custom CSS Here -->
+		<link href="css/style.css" rel="stylesheet"/>
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
@@ -37,7 +38,31 @@
 	</head>
 	<body>
 		<header>
+			<nav class="navbar navbar-inverse" role="navigation">
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Jack Reuter</a>
+					</div>
 
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+							<li class="active"><a href="#">Home</a></li>
+							<li><a href="#">About</a></li>
+							<li><a href="#">Portfolio</a></li>
+							<li><a href="#">Contact</a></li>
+							<!-- add to footer <li class="social pull-right"><a href="#">Social Links</a></li> -->
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</div><!-- /.container-fluid -->
+			</nav>
 		</header>
 		<main>
 			<section>
