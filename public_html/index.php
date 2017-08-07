ba7ce1880fc0dcbb2bae7529508a08803e335743
Dylan Reuter <djreuter45@gmail.com>
2017-08-07 16:05:16 -0600

    fixed image layout issue

--- public_html/index.php
+++ public_html/index.php
@@ -27,7 +27,7 @@
 		<![endif]-->
 
 		<!-- jQuery v3.0 -->
-		<script src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
@@ -37,72 +37,72 @@
 		<title>Jack Reuter | Online Portfolio</title>
 	</head>
 	<body>
-		<header>
-			<nav class="navbar navbar-inverse" role="navigation">
-				<div class="container-fluid">
-					<!-- Brand and toggle get grouped for better mobile display -->
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse"
-								  data-target="#bs-example-navbar-collapse-1">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand" href="#">Jack Reuter</a>
-					</div>
-
-					<!-- Collect the nav links, forms, and other content for toggling -->
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						<ul class="nav navbar-nav">
-							<li class="active"><a href="#">Home</a></li>
-							<li><a href="#">About</a></li>
-							<li><a href="#">Portfolio</a></li>
-							<li><a href="#">Contact</a></li>
-							<!-- add to footer <li class="social pull-right"><a href="#">Social Links</a></li> -->
-						</ul>
-					</div><!-- /.navbar-collapse -->
-				</div><!-- /.container-fluid -->
-			</nav>
-		</header>
-		<div class="picture"></div>
 		<main>
-			<section>
-				<div class="container about">
-					<div class="row">
-						<div class="col-md-10 col-md-offset-1">
-							<p>Pellentesque sed bibendum eros, vitae malesuada lorem. Ut posuere, purus id convallis gravida,
-								elit nunc commodo leo, quis euismod felis velit at orci. Ut sapien purus, condimentum eget arcu
-								nec, volutpat condimentum dui. Fusce gravida mauris et nisl vulputate iaculis. Orci varius
-								natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ultrices nisi
-								vel pulvinar finibus. Etiam egestas massa augue, vel dictum nisl mollis et. Curabitur sit amet
-								orci in lectus iaculis auctor eget vitae neque. Etiam ut risus at justo bibendum tempus vel
-								vulputate ligula. Etiam et nulla nec augue ultricies efficitur. Curabitur euismod, nisi iaculis
-								vestibulum facilisis, ligula justo commodo lacus, eget eleifend dolor eros pharetra neque. Duis
-								et metus lacus. Pellentesque rutrum risus vel tellus pharetra pretium.</p>
+			<header>
+				<nav class="navbar" role="navigation">
+					<div class="container-fluid">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle" data-toggle="collapse"
+									  data-target="#bs-example-navbar-collapse-1">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Jack Reuter</a>
 						</div>
-					</div>
-				</div><!-- container -->
-			</section>
-			<section>
-				<div class="container">
-					<div class="row">
-						<div class="col-md-10">
 
-						</div>
-					</div>
-				</div>
-			</section>
-			<section>
-				<div class="container">
-					<div class="row">
-						<div class="col-md-8">
-
-						</div>
-					</div>
-				</div>
-			</section>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav">
+								<li class="active"><a href="#">Home</a></li>
+								<li><a href="#">About</a></li>
+								<li><a href="#">Portfolio</a></li>
+								<li><a href="#">Contact</a></li>
+								<!-- add to footer <li class="social pull-right"><a href="#">Social Links</a></li> -->
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			</header>
 		</main>
+		<section>
+			<div class="container about">
+				<div class="row">
+					<div class="col-md-10 col-md-offset-1">
+						<p>Pellentesque sed bibendum eros, vitae malesuada lorem. Ut posuere, purus id convallis gravida,
+							elit nunc commodo leo, quis euismod felis velit at orci. Ut sapien purus, condimentum eget arcu
+							nec, volutpat condimentum dui. Fusce gravida mauris et nisl vulputate iaculis. Orci varius
+							natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ultrices nisi
+							vel pulvinar finibus. Etiam egestas massa augue, vel dictum nisl mollis et. Curabitur sit amet
+							orci in lectus iaculis auctor eget vitae neque. Etiam ut risus at justo bibendum tempus vel
+							vulputate ligula. Etiam et nulla nec augue ultricies efficitur. Curabitur euismod, nisi iaculis
+							vestibulum facilisis, ligula justo commodo lacus, eget eleifend dolor eros pharetra neque. Duis
+							et metus lacus. Pellentesque rutrum risus vel tellus pharetra pretium.</p>
+					</div>
+				</div>
+			</div><!-- container -->
+		</section>
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10">
+
+					</div>
+				</div>
+			</div>
+		</section>
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+
+					</div>
+				</div>
+			</div>
+		</section>
+
 		<footer>
 			<div class="container">
 				<div class="row">
