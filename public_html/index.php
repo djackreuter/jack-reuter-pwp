57ca4e8da508e16603f29b4ae36deafc0b0c053c
Dylan Reuter <djreuter45@gmail.com>
2017-08-13 16:58:56 -0600

    corrected js link

--- public_html/index.php
+++ public_html/index.php
@@ -9,7 +9,9 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.20/jquery.fancybox.min.css" type="text/css">
+		<!-- Fancybox CSS -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.20/jquery.fancybox.min.css"
+				 type="text/css">
 
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
@@ -31,7 +33,9 @@
 		<!-- jQuery v3.0 -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
-		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.20/jquery.fancybox.min.js"
+		<!-- Fancybox jQuery -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.20/jquery.fancybox.min
+		.js"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
@@ -103,7 +107,7 @@
 				<div class="row">
 					<div class="col-sm-6 col-md-4">
 						<div class="thumbnail">
-							<a href="documentation/images/acme.png" data-fancybox="group" title="View img in new window"><img
+							<a href="documentation/images/acme.png" data-fancybox="images"><img
 									src="documentation/images/acme-small.png" alt="acme web design"/></a>
 							<div class="caption">
 								<h3>Acme Web Design</h3>
@@ -112,8 +116,7 @@
 					</div><!-- /.col-sm-6 col-md-4 -->
 					<div class="col-sm-6 col-md-4">
 						<div class="thumbnail">
-							<a href="documentation/images/placeholder-2.jpg" data-fancybox="group" title="View img in new
-							window">
+							<a href="documentation/images/placeholder-2.jpg" data-fancybox="images">
 								<img src="documentation/images/placeholder-2-small.jpg" alt="place holder
 								picture"/></a>
 							<div class="caption">
--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,1 +1,8 @@
 $('body').scrollspy({target: '#navbar'});
+
+$('[data-fancybox]').fancybox({
+	loop: false,
+	margin: [44,0],
+	keyboard: true,
+	arrows: true
+});
