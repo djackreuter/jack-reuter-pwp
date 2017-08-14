1346738f275e9baed4ac187be9b46679e56aeb35
Dylan Reuter <djreuter45@gmail.com>
2017-08-14 07:46:27 -0600

    replaced regular LightBox js and css files with minified versions

--- public_html/index.php
+++ public_html/index.php
@@ -10,7 +10,7 @@
 				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 		<!-- LightBox CSS -->
-		<link href="css/lightbox.css" rel="stylesheet">
+		<link href="css/lightbox.min.css" rel="stylesheet">
 
 		<!-- Optional theme -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
@@ -33,11 +33,7 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
 		<!-- LightBox JavaScript -->
-		<script type="text/javascript" src="js/lightbox.js"></script>
-
-		<!-- Fancybox jQuery -->
-		<!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.1.20/jquery.fancybox.min
-		.js"></script> -->
+		<script type="text/javascript" src="js/lightbox.min.js"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
