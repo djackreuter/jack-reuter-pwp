ee84c1d1913d4fe4552a94109804e531ca659ff3
Jack Reuter <djreuter45@gmail.com>
2018-03-17 13:33:32 -0600

    beginning an edit to simplify site

--- public_html/index.php
+++ public_html/index.php
@@ -4,7 +4,6 @@
 		<meta charset="UTF-8"/>
 		<meta http-equiv="X-UA-Compatable" content="IE=edge"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
 				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -81,108 +80,26 @@
 	</head>
 	<body class="sfooter" data-spy="scroll" data-target="#nav-links">
 		<div class="sfooter-content">
-			<header>
-				<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-					<div class="container-fluid">
-						<!-- Brand and toggle get grouped for better mobile display -->
-						<div class="navbar-header">
-							<!-- button went here -->
-							<a class="navbar-brand hidden-xs" id="title" href="#">Jack Reuter</a>
-						</div>
-
-						<!-- Collect the nav links, forms, and other content for toggling -->
-						<div id="nav-links"><!-- collapse navbar-collapse -->
-							<ul class="nav navbar-nav">
-								<li><a href="#home">Home</a></li>
-								<li><a href="#about">About</a></li>
-								<li><a href="#portfolio">Portfolio</a></li>
-								<li><a href="#contact">Contact</a></li>
-							</ul>
-						</div><!-- /.navbar-collapse -->
-					</div><!-- /.container-fluid -->
-				</nav>
-			</header>
-			<section id="home">
-
-			</section>
 			<section id="about">
 				<div class="container about">
-					<div class="row">
-						<div class="col-md-4 about-header">
-							<h2>About Me</h2>
-						</div><!-- /.col-md-4 -->
-					</div><!-- /.row -->
-					<div class="row">
-						<div class="col-md-3 col-xs-12">
-							<img class="img img-circle" src="images/about-me.jpg" alt="me"/>
+					<div class = "well">
+						<div class="row">
+							<div class="col-md-3 col-xs-12">
+								<img class="img img-circle" src="images/about-me.jpg" alt="me"/>
+							</div>
 						</div>
-						<div class="col-md-9">
-							<p>Hi I'm Jack Reuter, I am a full-stack web developer born and raised in Albuquerque, NM. I
-								attended The University of New Mexico and received my B.A. in Communication in 2016. Upon
-								graduation, I decided that my passion was not in the field of communication, but rather in
-								the world of software development. I attended the Deep Dive Coding bootcamp in Albuquerque
-								where I gained solid knowledge of essential web technologies, and built sites utilizing both
-								frontend and backend languages. I have experience working with HTML5, CSS3, JavaScript, PHP,
-								and MySQL, as well as popular libraries and frameworks such as Bootstrap, jQuery, and Angular.
-								In my free time I enjoy spending time outdoors and enjoying the New Mexico weather. I also
-								love watching soccer, and am a strong supporter of Manchester City and the Seattle Sounders.
-							</p>
-						</div>
-					</div><!-- /.row -->
+						<div class = "row">
+							<div class="col-md-9">
+								<p>Hi I'm Jack Reuter, Linux enthusiast and full-stack developer in Santa Fe, NM. I use Ubuntu as
+									my daily driver, and work primarily with Ruby and Javascript. I also have experience with PHP,
+									and other Linux distro's like Fedora and Debian. In my free time I enjoy spending time
+									outdoors. I also love watching soccer, and am a strong supporter of Manchester City and the
+									Seattle Sounders.
+								</p>
+							</div>
+						</div><!-- /.row -->
+					</div>
 				</div><!-- /.container -->
-			</section>
-			<section id="portfolio">
-				<div class="container projects">
-					<div class="row">
-						<div class="col-md-4">
-							<h2>Portfolio</h2>
-						</div><!-- /.col-md-4 -->
-					</div><!-- /.row -->
-					<div class="row">
-						<div class="col-sm-6 col-md-4">
-							<!-- thumbnail images are 274 x 163 -->
-							<div class="thumbnail">
-								<a href="images/acme.png" data-lightbox="images"><img
-										src="images/acme-home-small.png" alt="acme web design"/></a>
-								<div class="caption">
-									<h3>Acme Web Design</h3>
-								</div><!-- /.caption -->
-							</div><!-- /.thumbnail -->
-						</div><!-- /.col-sm-6 col-md-4 -->
-
-						<div class="col-sm-6 col-md-4">
-							<!-- thumbnail images are 274 x 163 -->
-							<div class="thumbnail">
-								<a href="images/deepDiveTutor.png" data-lightbox="images">
-									<img src="images/deepDiveTutor-small.png" alt="deep dive tutor"/></a>
-								<div class="caption">
-									<h3>Deep Dive Tutor</h3>
-								</div><!-- /.caption -->
-							</div><!-- /.thumbnail -->
-						</div><!-- /.col-sm-6 col-md-4 -->
-
-						<div class="col-sm-6 col-md-4">
-							<!-- thumbnail images are 274 x 163 -->
-							<div class="thumbnail">
-								<a href="images/todo-list.png" data-lightbox="images">
-									<img src="images/todo-list-small.png" alt="to-do list"/></a>
-								<div class="caption">
-									<h3>To-Do List</h3>
-								</div><!-- /.caption -->
-							</div><!-- /.thumbnail -->
-						</div><!-- /.col-sm-6 col-md-4 -->
-
-						<div class="col-sm-6 col-md-4">
-							<div class="thumbnail">
-								<a href="https://githubfinder.jackreuter
-								.com" target="_blank"><img src="images/github-finder-small.png" alt="github finder"/></a>
-								<div class="caption">
-									<h3>Github Finder</h3>
-								</div><!-- /.caption -->
-							</div><!-- /.thumbnail -->
-						</div><!-- /.col-sm-6 col-md-4 -->
-					</div><!-- /.row -->
-				</div><!-- /. container projects -->
 			</section>
 			<section id="contact">
 				<div class="container contact">
